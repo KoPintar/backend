1b05df901c68032b48e9aa5029765a9224fe409d
Jagad R R <43224845+zenthicmc@users.noreply.github.com>
2024-06-01 12:10:25 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -29,6 +29,6 @@ import AuthRoute from "./Routes/AuthRoute";
   });
   
   app.listen(4000, () =>
-    console.log(`⚡️ Server is running at http://localhost:3000`)
+    console.log(`⚡️ Server is running at http://localhost:4000`)
   );
 })();
